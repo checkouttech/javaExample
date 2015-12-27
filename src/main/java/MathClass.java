3fa728fac20ff094380374a1f50915e4868071c2
Kapil Mahant <kmahant@la-mac-kmahant2.rubicorp.com>
2015-12-27 13:55:12 -0800

    java doc

--- src/main/java/MathClass.java
+++ src/main/java/MathClass.java
@@ -3,11 +3,27 @@ package src.main.java;
 //package codedir;
 
 public class MathClass {
-
     /**
-     * @param args
+     * ## The add number function
+     *
+     * This method doesn't do much, except for adding 2 numbers 
+     *
+     *  * Generates a random string at a given length, `length`
+     *  * Uses only characters in the range given by `from` and `to`.
+     *
+     * Example:
+     *
+     * ```java
+     * randomString(new Random(), 'a', 'z', 10);
+     * ```
+     * @param a    the first number to add  
+     * @param b    the second number to add
+     * @param length the length of the generated string
+     * @return      the sum of `a` and `b`
      */
     
+
+
     public int addNumbers ( int a , int b )
     {
         return  a + b ;
